799fe5b7806c8e620dd7b595c3806f784679628c
mysteroak <leandereicher.info@gmail.com>
2025-05-13 07:32:05 +0200

    + oakstation ssh key

--- hosts/common/users/mysteroak.nix
+++ hosts/common/users/mysteroak.nix
@@ -8,7 +8,9 @@
       "networkmanager"
       "wheel"
     ];
-    openssh.authorizedKeys.keys = [];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEG7T8kPc7dQ0JGq4EYf8usi9TiZJQFrn9iAH+gydbH mysteroak@oakstation"
+    ];
     packages = [inputs.home-manager.packages.${pkgs.system}.default];
   };
 }
